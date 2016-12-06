837269e90478d1e9b443cf0b55f10438a0b3991e
yanx611 <yanx611@gmail.com>
2016-12-05 21:41:20 -0500

    email working, api working, schedule working, error checking needed

--- public/resources/main.js
+++ public/resources/main.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     $("#round").change(function() {
 
         if(this.checked) {
-
+            console.log("Return data field open!");
             $("#rD").removeClass('ui disabled input').addClass('ui input');
             $("#rY").removeClass('ui disabled input').addClass('ui input');
             $("#retMonth").removeClass('ui disabled dropdown').addClass('ui dropdown');
 
         } else {
-
+            console.log("Return data field close!");
             $("#rD").removeClass('ui input').addClass('ui disabled input');
             $("#rY").removeClass('ui input').addClass('ui disabled input');
             $("#retMonth").removeClass('ui dropdown').addClass('ui disabled dropdown');
@@ -20,39 +20,114 @@ $(document).ready(function(){
     });
 
 
-    //send the user data to server
-    $("#sub").click(function() {
+    function validateForm() {
+        numdays = [31,28,31,30,31,30,31,31,30,31,30,31];
+        namemonth = ["January","Feburary","March","April","May","June","July","August","September","October","November","December"];
 
+        if ($("#dpYear").val() == "" || $("#dpMonth").val() == "" ||  $("#dpDay").val() == "" || $("#origin").val() == "" || $("#des").val() == "" || $('#num').val() == "" || $("#email").val() == "" || $("#waiting").val() == "") {
+            console.log("not fill in all the blanks")
+            alert("Please fill in all fields");
+            return false;
+        }
+        console.log("fields valid!")
+        var today = new Date();
+        if (parseInt($("#dpYear").val()) < today.getFullYear()) {
+            alert("You cannot check past ticket's value");
+            return false;
+        } else {
+            if (parseInt($("#dpYear").val()) == today.getFullYear()) {
+                if (parseInt($("#dpMonth").val()) < today.getMonth()+1 ) {
+                    alert("You cannot check past ticket's value");
+                    return false;
+                } else {
+                    if (parseInt($("#dpMonth").val()) ==  today.getMonth()+1 ) {
+                        if (parseInt($("#dpDay").val()) < today.getDate()) {
+                            alert("You cannot check past ticket's value");
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        console.log("departure date valid!")
 
-
-        console.log("clicked submit");
-
-        var rq = {};
-        rq.origin = $("#origin").val();
-        rq.destination = $("#des").val();
-        rq.dpdate = $("#dpYear").val()+'-'+$("#dpMonth").val()+'-'+$("#dpDay").val();
-        rq.waiting = parseInt(parseFloat($("#waiting").val())*60);
-        rq.num = parseInt($('#num').val());
-        rq.email = $("#email").val();
-        rq.round = 0;
-        if ($("#round").checked) {
-            rq.round = 1;
-            rq.retdate = $("#retYear").val()+'-'+$("#retMonth").val()+'-'+$("#retDay").val();
+        if ($("#round").is(':checked')) {
+            console.log("roundtrip checked!")
+            if ($("#retYear").val() == "" || $("#retMonth").val() == "" || $("#retDay").val() == "" ) {
+                alert("please enter return date");
+                return false;
+            }
+            if (parseInt($("#retYear").val()) < parseInt($("#dpYear").val())) {
+                alert("Return date cannot be before departure date.");
+                return false;
+            } else {
+                if (parseInt($("#retYear").val()) == parseInt($("#dpYear").val())) {
+                    if (parseInt($("#retMonth").val()) < parseInt($("#dpMonth").val()) ) {
+                        alert("Return date cannot be before departure date.");
+                        return false;
+                    } else {
+                        if (parseInt($("#retMonth").val()) ==  parseInt($("#dpMonth").val()) ) {
+                            if (parseInt($("#retDay").val()) < parseInt($("#dpDay").val())) {
+                                alert("Return date cannot be before departure date.");
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-    	$.ajax({
-			type: "POST",
-			url: "http://localhost:4000/user",
-			dataType: 'json',
-            contentType: 'application/json',
-			data: JSON.stringify(rq),
-			success: function(data) {
-				alert("Data goes into our system!");
-			},
-			error: function(error) {
-				console.log(error);
-				alert("Unable to send!");
-			}
-		});
+        console.log("return date valid!")
+        if ($("#dpMonth").val() == "2" && parseInt($("#dpYear".val()))%4 == 0 && parseInt($("#dpYear".val()))%100 != 0) {
+            if (parseInt($("#dpDay".val())) > 29) {
+                alert(namemonth[parseInt($("#dpMonth").val())-1]+" does not have more than 29 days");
+                return false;
+            }
+        } else {
+
+            var m = parseInt($("#dpMonth").val());
+            if ( parseInt($("#dpDay").val()) > numdays[m-1]) {
+                alert(namemonth[m-1]+" does not have more than "+numdays[m-1]+" days");
+                return false;
+            }
+        }
+        return true;
+    }
+    console.log("month-date valid!")
+
+
+    //send the user data to server
+    //not using the form submit function as the it will not reveive the data
+    $("#sub").click(function() {
+
+        if (validateForm()) {
+            var rq = {};
+            rq.origin = $("#origin").val();
+            rq.destination = $("#des").val();
+            rq.dpdate = $("#dpYear").val()+'-'+$("#dpMonth").val()+'-'+$("#dpDay").val();
+            rq.waiting = parseInt(parseFloat($("#waiting").val())*60);
+            rq.num = parseInt($('#num').val());
+            rq.email = $("#email").val();
+            rq.round = 0;
+            if ($("#round").is(':checked')) {
+                rq.round = 1;
+                rq.retdate = $("#retYear").val()+'-'+$("#retMonth").val()+'-'+$("#retDay").val();
+            }
+            console.log("data post to server formed!");
+        	$.ajax({
+    			type: "POST",
+    			url: "http://localhost:4000/user",
+    			dataType: 'json',
+                contentType: 'application/json',
+    			data: JSON.stringify(rq),
+    			success: function(data) {
+    				alert("Data goes into our system!");
+    			},
+    			error: function(error) {
+    				console.log(error);
+    				alert("Unable to send!");
+    			}
+    		});
+        }
     });
 });
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var easyjson = require('easyjson');
 var http = require('http').Server(app);
 var fs = require('fs');
 var request = require('request');
-var mailer = require('nodemailer');
 var CronJob = require('cron').CronJob;
+var mailer   = require("emailjs");
 
 app.use(express.static('public'));
 app.use(bodyParser());
@@ -16,75 +15,80 @@ app.get('/', function(req, res) {
 });
 
 app.post('/user', function(req, res) {
+    //send back server response
     res.send(req.body);
     pushUser(req.body);
 });
 
 
 function pushUser(data) {
-    console.log(data.origin);
     fs.readFile('data/user.json', 'utf8', function readFileCallback(err, file){
         if (err){
             console.log(err);
         } else {
-        obj = JSON.parse(file);
-        var p;
-        if (data.round == 0) {
-            var p =
-            {
-                "email" : data.email,
-                "FlightRequest": {
-                    "request": {
-                        "slice":[
-                            {
-                                "origin":data.origin,
-                                "destination":data.destination,
-                                "date":data.dpdate,
-                                "maxConnectionDuration":data.waiting
-                            }
-                        ],
-                        "passengers": {
-                            "adultCount":data.num
-                        },
-                        "solutions":1
-                    }
-                }
-            };
-        } else {
-            var p =
-            {
-                "email" : data.email,
-                "FlightRequest": {
-                    "request": {
-                        "slice":[
-                            {
-                                "origin":data.origin,
-                                "destination":data.destination,
-                                "date":data.dpdate,
-                                "maxConnectionDuration":data.waiting
+            //open file convert it to object
+            obj = JSON.parse(file);
+            var p;
+            //requests varies depend on whether roundtrip
+            if (data.round == 0) {
+                var p =
+                {
+                    "email" : data.email,
+                    "FlightRequest": {
+                        "request": {
+                            "slice":[
+                                {
+                                    "origin":data.origin,
+                                    "destination":data.destination,
+                                    "date":data.dpdate,
+                                    "maxConnectionDuration":data.waiting
+                                }
+                            ],
+                            "passengers": {
+                                "adultCount":data.num
                             },
-                            {
-                                "origin":data.destination,
-                                "destination":data.origin,
-                                "date":data.retdate,
-                                "maxConnectionDuration":data.waiting
-                            }
-                        ],
-                        "passengers": {
-                            "adultCount":data.num
-                        },
-                        "solutions":1
+                            "solutions":1
+                        }
                     }
-                }
-            };
+                };
+            } else {
+                var p =
+                {
+                    "email" : data.email,
+                    "FlightRequest": {
+                        "request": {
+                            "slice":[
+                                {
+                                    "origin":data.origin,
+                                    "destination":data.destination,
+                                    "date":data.dpdate,
+                                    "maxConnectionDuration":data.waiting
+                                },
+                                {
+                                    "origin":data.destination,
+                                    "destination":data.origin,
+                                    "date":data.retdate,
+                                    "maxConnectionDuration":data.waiting
+                                }
+                            ],
+                            "passengers": {
+                                "adultCount":data.num
+                            },
+                            "solutions":1
+                        }
+                    }
+                };
+            }
+            //add some new user
+            obj.users.push(p);
+            //convert it back to json
+            json = JSON.stringify(obj);
+            // write it back
+            fs.writeFile('data/user.json', json, 'utf8', function(err) {
+                console.log("user data has stored.");
+            });
         }
-
-        obj.users.push(p); //add some new user
-        json = JSON.stringify(obj); //convert it back to json
-        fs.writeFile('data/user.json', json, 'utf8', function(err) {
-            console.log("ok");
-        }); // write it back
-    }});
+    });
 }
 
 
@@ -115,6 +119,7 @@ function reqAPIdata(data,email) {
     resp.on('update', function () {
         console.log(resp.mail);
         console.log(resp.mes);
+        sendEmail(resp.mail,resp.mes);
     });
 }
 
@@ -131,32 +136,31 @@ function formatData(data) {
     return output;
 }
 
-// function sendEmail(add, mes) {
-//     var smtpTransport = mailer.createTransport('SMTP', {
-//         service: 'Gmail',
-//         auth: {
-//             user: "flightonight2016@gmail.com",
-//             pass: "FlighT@rpi2016"
-//         }
-//     });
-//     var mail = {
-//         from: 'FlighTonight<flightonight2016@gmail.com>',
-//         to: add,
-//         subject: 'Lowest price for your trip from FlighTonight',
-//         text: mes
-//     }
-//     smtpTransport.sendMail(mail, function(err, res) {
-//         console.log(mail);
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log("message send: " + res.message);
-//         }
-//         // smtpTransport.close();
-//     });
-// }
+function sendEmail(add, mes) {
+    //function to send wmail that using our Google account, might need a secure way to do this hhh
+    console.log("start sending mail to: "+add);
+    var server = mailer.server.connect({
+        user:"flightonight2016@gmail.com",
+        password:"FlighT@rpi2016",
+        host:"smtp.gmail.com",
+        ssl: true
+    });
+    server.send({
+        text: mes,
+        from: "FlighTonight<flightonight2016@gmail.com>",
+        to: add,
+        subject: "Today's lowest price for your trip from FlighTonight"
+    }, function(err, message) {
+        console.log(err||message);
+    });
+
+
+
+    console.log("has been sent!");
+}
 
 function executewhole() {
+    //read file and pass to api
     var list = JSON.parse(fs.readFileSync('data/user.json', 'utf8'));
     list['users'].forEach(function(i) {
         var d = i['FlightRequest'];
@@ -165,21 +169,21 @@ function executewhole() {
     });
 }
 
-
 function schedule(){
     var job = new CronJob({
-      cronTime: '10 * * * * *',
-      onTick: function() {
-        executewhole();
-      },
-      start: false,
-      timeZone: 'America/New_York'
+        // this job should run everyday at 11:00am
+        cronTime: '30 * * * * *',
+        onTick: function() {
+            executewhole();
+        },
+        start: false,
+        timeZone: 'America/New_York'
     });
     job.start();
 }
 
 schedule();
-
+// executewhole();
 app.listen(4000, function(){
   console.log('Server up on *:4000');
 });
